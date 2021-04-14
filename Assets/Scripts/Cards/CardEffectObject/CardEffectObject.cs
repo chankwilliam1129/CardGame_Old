e3c47699e78b3615675931c13bbec07606dc6203
chankwilliam1129 <chankwilliam1129@gmail.com>
2021-04-14 21:58:14 +0900

    0414_2200

---
+++ Assets/Scripts/Cards/CardEffectObject/CardEffectObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardEffectObject : MonoBehaviour
+{
+    public Vector2Int value;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
