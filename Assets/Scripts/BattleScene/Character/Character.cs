03e1da6e8efda2a2193b30c9fc6f27eabf73ef99
chankwilliam1129 <chankwilliam1129@gmail.com>
2021-04-21 15:05:45 +0900

    Revert "Merge branch 'master' of https://github.com/chankwilliam1129/NewCardGame"
    This reverts commit d582a1eb1d47be1a2147d777d6ff9d9e91eb0ade, reversing
    changes made to 3df6d787d59395e6b2bb0ca7005f9c43cca7bd31.

--- Assets/Scripts/BattleScene/Character/Character.cs
+++ Assets/Scripts/BattleScene/Character/Character.cs
@@ -15,18 +15,6 @@ public class DamageData
     public Character from;
 }
 
-public class HealData
-{
-    public HealData(int d, Character c)
-    {
-        heal = d;
-        from = c;
-    }
-
-    public int heal;
-    public Character from;
-}
-
 public class Character : MonoBehaviour
 {
     public CharacterEvent characterEvent;
@@ -63,13 +51,6 @@ public class Character : MonoBehaviour
     public void LoseHealthPoint(int hp)
     {
         healthPoint -= hp;
-        OnHealthChanged?.Invoke(this, EventArgs.Empty);
-    }
-    public void HealHealthPoint(int hp)
-    {
-        if(GetHealthPointMax() > healthPoint)
-        healthPoint += hp;
-
         OnHealthChanged?.Invoke(this, EventArgs.Empty);
     }
 
