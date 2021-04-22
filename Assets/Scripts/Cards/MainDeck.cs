ea5c2548a168aaf9bd1b591b61bc315321990c21
chankwilliam1129 <chankwilliam1129@gmail.com>
2021-04-22 18:50:09 +0900

    0422_1850

--- Assets/Scripts/Cards/MainDeck.cs
+++ Assets/Scripts/Cards/MainDeck.cs
@@ -8,15 +8,12 @@ public class MainDeck : ScriptableObject
     public CardDatabase cardDatabase;
     public List<CardBattleData> deck = new List<CardBattleData>();
 
-    private int randomNumber;
-
     public void CreateNewCard()
     {
         deck.Clear();
         for (int i = 0; i < 20; i++)
         {
-            randomNumber = Random.Range(0, cardDatabase.cardList.Count);
-            deck.Add(cardDatabase.cardList[randomNumber].battleData);
+            deck.Add(cardDatabase.cardList[i].battleData);
         }
     }
 }
