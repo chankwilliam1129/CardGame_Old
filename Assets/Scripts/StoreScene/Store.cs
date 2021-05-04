c0dcaca11a22962593e17902f50f755281ee62ab
I-my-me-mine <79188868+I-my-me-mine@users.noreply.github.com>
2021-05-04 18:33:16 +0900

    0504_1833

--- Assets/Scripts/StoreScene/Store.cs
+++ Assets/Scripts/StoreScene/Store.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Store : CardDisplayOnlyGroup
 {
     public CardDatabase cardDatabase;
     public CardDisplay[] sellCard = new CardDisplay[5];
+
+    public Button buyButton;
+    public Button canselButton;
+
+    public CardDisplay SelectCard;
 
     private void Start()
     {
@@ -14,18 +20,25 @@ public class Store : CardDisplayOnlyGroup
             sellCard[i].data = cardDatabase.cardList[Random.Range(0, cardDatabase.cardList.Count)].battleData;
             sellCard[i].SetUp();
         }
+ 
+        buyButton.gameObject.SetActive(false);
+        canselButton.gameObject.SetActive(false);
     }
 
     public override void OnPointEnter(CardDisplay card) 
     {
-
+        card.GetComponent<Animator>().SetBool("isTouch", true);
     }
 
     public override void OnPointExit(CardDisplay card)
     {
+        card.GetComponent<Animator>().SetBool("isTouch", false);
     }
 
     public override void OnClick(CardDisplay card)
     {
+        SelectCard = card;
+        buyButton.gameObject.SetActive(true);
+        canselButton.gameObject.SetActive(true);
     }
 }
