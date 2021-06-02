5181faebc8ad758db3a98efe7564dbbb348cf1b0
ryuzin135 <maeda-0426@ezweb.ne.jp>
2021-06-02 18:55:36 +0900

    0602_1855

--- Assets/Scripts/Cards/PlayerData.cs
+++ Assets/Scripts/Cards/PlayerData.cs
@@ -43,6 +43,7 @@ public class PlayerData : ScriptableObject
         shieldClear = startShieldClear;
         curHealth = health;
         coin = startCoin;
+        relic.Clear();
         MapData.Instance.StartNewGame();
     }
 
